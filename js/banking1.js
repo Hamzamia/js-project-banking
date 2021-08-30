d469b05f8f5fae0d775ba330bf95db20bcfe8224
Hamzammia <mahmudul.web.developer@gmail.com>
2021-08-30 21:16:58 +0600

    add

--- js/banking1.js
+++ js/banking1.js
@@ -20,9 +20,46 @@ document.getElementById('deposite-button').addEventListener('click', function ()
     const newTotalBalance = previousBalanceTotal + newdepositAmount;
     balanceTotal.innerText = newTotalBalance;
 
+
+
+
+
+
     depositInput.value = '';
 
 
 
+})
+
+
+//balance withdraw 
+
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawbalanceText = withdrawInput.value;
+    const newWithdraw = parseFloat(withdrawbalanceText);
+    console.log(newWithdraw);
+
+
+    //sset withdraw total
+
+    const withdrawtotal = document.getElementById('withdraw-total');
+    const previousWithdrawText = withdrawtotal.innerText;
+    const previousWithdraw = parseFloat(previousWithdrawText);
+    const withdrawTotal = newWithdraw + previousWithdraw;
+    withdrawtotal.innerText = withdrawTotal;
+
+
+
+    //set total balance after withbdraw
+
+    const balanceTotal = document.getElementById('total-amount');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    const newTotalBalance = previousBalanceTotal - newWithdraw;
+    balanceTotal.innerText = newTotalBalance;
+
+    withdrawInput.value = '';
 })
 
